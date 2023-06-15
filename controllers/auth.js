6feefe55b9fe08f5826e546b86f34c8742db820d
Keyur Raval <ravalkeyur054@gmail.com>
2023-06-15 12:04:08 +0530

    changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,11 +11,13 @@ const registerUser = async (req, res) => {
         const user = await User.create({ handle, email, password, role: category, links: [defaultLink] });
         const token = jwt.sign({ email: email }, process.env.SECRET_KEY);
         console.log('user', user);
+        res.setHeader("Access-Control-Allow-Origin", "*");
         return res.json({ message: 'User Created', status: 'success', 'token': token, id: user._id })
     } catch (err) {
         if (err.code === '11000') {
             return res.json({ message: "Try different email", status: 'error' });
         }
+        res.setHeader("Access-Control-Allow-Origin", "*");
         return res.json({ message: err.message, status: 'error' });
     }
 }
@@ -26,11 +28,15 @@ const loginUser = (req, res) => {
         const user = User.findOne({ email: email, password: password });
         console.log(user);
         if (!user) {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+
             return res.json({ status: 'not found', error: 'Invalid credentials' })
         }
         const token = jwt.sign({ email: email }, process.env.SECRET_KEY);
         return res.json({ message: 'user found', status: 'success', 'token': token, id: user._id });
     } catch (err) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+
         return res.json({ message: err.message, status: 'error' });
     }
 }
