7595d78164f0788290e9c43c5846278200a1248a
Keyur Raval <ravalkeyur054@gmail.com>
2023-06-15 11:56:29 +0530

    changes

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ env.config();
 mongoose.set('strictQuery', false);
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+}))
+
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("Connected");
